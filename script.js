20b99957d2d042dd99febd7a8f6e258f95a4f7be
kiama-maxwel <kiamamaxwel@gmail.com>
2024-05-16 00:10:11 +0200

    added some codes to the script.js file

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const promise = fetch('https://restcountries.com/v3.1/all');
 promise.then((res) => res.json()).then((data) => console.log(data));
+
+
+
+function handleClick() {
+    alert('Button clicked!');
+}
+
+var button = document.getElementById('search-button');
+
+button.addEventListener('click', handleClick);
